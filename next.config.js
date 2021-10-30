ffb072d76c7f24803e92224d79eae8423eb96a0a
moka-ayumu <10963468+moka-ayumu@users.noreply.github.com>
2021-10-31 06:38:32 +0900

    Applied 'exportPathMap'

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   reactStrictMode: true,
   basePath: "/chume_debut",
+  exportPathMap: async function () {
+    return {
+      "/": { page: "/" },
+      "/main": { page: "/[pid]" },
+      "/description": { page: "/[pid]" },
+      "/contact": { page: "/[pid]" },
+      "/debut": { page: "/[pid]" },
+    };
+  },
 };
